caff0f7ece6eb56968e8525fe2a00c9842425d86
BrendoDCruz <158388412+BrendoDCruz@users.noreply.github.com>
2025-05-25 16:00:01 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -6,7 +6,11 @@ int main() {
     int bispo = 5;
     int rainha = 8;
 
-    int i;
+    int baixo = 2;
+    int esquerda = 1;
+    int cavalo = 3;
+
+    int i, j;
 
     printf("Movimento da Torre:\n");
     for (i = 0; i < torre; i++) {
@@ -19,13 +23,30 @@ int main() {
         printf("Cima, Direita\n");
         i++;
     }
-    
+
     printf("\nMovimento da Rainha:\n");
     i = 0;
     do {
         printf("Esquerda\n");
         i++;
     } while (i < rainha);
+
+    printf("\nMovimento do Cavalo:\n");
+    for (i = 0; i < cavalo; i++) {
+        j = 0;
+        while (j < baixo) {
+            printf("Baixo\n");
+            j++;
+        }
+
+        j = 0;
+        while (j < esquerda) {
+            printf("Esquerda\n");
+            j++;
+        }
+
+        printf("---\n");
+    }
 
     return 0;
 }
